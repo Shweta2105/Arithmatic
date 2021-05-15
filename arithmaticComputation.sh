4905598fb601c05be6722036af03dc32a0f35f74
Shweta2105 <sghadi21@gmail.com>
2021-05-15 07:22:04 +0530

    Added: Stored results

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -15,3 +15,10 @@ echo $result3
 result4=$(($a%$b+$c))
 echo $result4
 
+declare -A Results
+Results[Result1]="( $result1 )"
+Results[Result2]="( $result2 )"
+Results[Result3]="( $result3 )"
+Results[Result4]="( $result4 )"
+
+echo All Computation Results : ${Results[@]}
