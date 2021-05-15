d0a7293129146149745302afab65658e49371e0a
Shweta2105 <sghadi21@gmail.com>
2021-05-15 06:58:21 +0530

    Added: Third computation

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -4,8 +4,10 @@ echo "Enter the  three inputs ::"
 read a b c
 
 result1=$(($a+$b*$c))
-
 echo $result1
 
-result2-$(($a*$b+$c))
+result2=$(($a*$b+$c))
 echo $result2
+
+result3=$(($c+$a/$b))
+echo $result3
